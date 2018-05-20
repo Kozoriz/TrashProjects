ab73e4843838aa909963a61789c0a44a0bf100e5
Andrii Kozoriz <kozorizandriy@gmail.com>
2018-05-20 22:32:36 +0300

    implement youtube downloading

--- Django_labs/Django_labs/apps/YoutubeDownloader/urls.py
+++ Django_labs/Django_labs/apps/YoutubeDownloader/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('loaded_files/', views.downloaded_page, name='loaded_files'),
 ]
--- Django_labs/Django_labs/apps/YoutubeDownloader/views.py
+++ Django_labs/Django_labs/apps/YoutubeDownloader/views.py
@@ -1,6 +1,87 @@
 from django.shortcuts import render
+from django import forms
+from pytube import YouTube
+from pytube import Playlist
+import os
+import urllib
+import re
+import logging
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+
+
+class UrlInputForm(forms.Form):
+    dir = forms.CharField(widget=forms.TextInput, initial="C:\\YoutubeDownloaded\\", label="Save directory")
+    video_urls = forms.CharField(widget=forms.Textarea, label="Video links", required=False)
+    playlist_urls = forms.CharField(widget=forms.Textarea, label="Playlists links", required=False)
+    group_links = forms.CharField(widget=forms.Textarea,
+                                  label="Groups links(links to profile! Like: https://www.youtube.com/user/woebyBright",
+                                  required=False)
 
 
 def index(request):
-    context = {}
-    return render(request, "YoutubeDownloader/index.html", context)
+    form = UrlInputForm()
+    return render(request, "YoutubeDownloader/index.html", {'form': form})
+
+
+def downloaded_page(request):
+    directory = request.POST.get('dir')
+
+    urls_data = request.POST.get('video_urls')
+    urls = urls_data.split('\r\n')
+
+    playlists = request.POST.get('playlist_urls')
+    playlist_urls = playlists.split('\r\n')
+
+    groups = request.POST.get('group_links')
+    group_links = groups.split('\r\n')
+
+    try:
+        download_urls(urls, directory)
+    except:
+        pass
+
+    try:
+        for playlist in playlist_urls:
+            pl = Playlist(url=playlist)
+            pl_dir = directory + playlist.split("list=")[1] + "\\"
+            if not os.path.exists(pl_dir):
+                os.makedirs(pl_dir)
+            pl.download_all(download_path=pl_dir)
+    except:
+        pass
+
+    try:
+        for group in group_links:
+            group_link_parts = group.split('/')
+            if group_link_parts[len(group_link_parts)-2] != 'user':
+                continue
+            user = group_link_parts[len(group_link_parts)-1]
+            group_dir = directory + user
+            group += '/videos'
+            html_content = urllib.request.urlopen(group)
+            video_codes = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+            video_codes = set(video_codes)
+            group_videos = []
+            for code in video_codes:
+                group_videos.append("https://www.youtube.com/watch?v=" + code)
+            download_urls(group_videos, group_dir)
+    except:
+        pass
+
+    return render(request, "YoutubeDownloader/loaded_files.html", {'data': directory})
+
+
+def download_urls(urls, directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    for url in urls:
+        yt = YouTube(url=url)
+        yt.streams \
+            .filter(progressive=True, file_extension='mp4') \
+            .order_by('resolution') \
+            .desc() \
+            .first() \
+            .download(directory)
